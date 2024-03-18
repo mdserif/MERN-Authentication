ae933308314000ce4b1b888e3843ea5d3c07f273
Md Serif <mdserif1998@gmail.com>
2024-03-18 20:41:50 +0530

    circular progressbar is added in sendpasswordresetemail endpoint

--- reactAuth/src/pages/Auth/SendPasswordResetEmail.js
+++ reactAuth/src/pages/Auth/SendPasswordResetEmail.js
@@ -1,4 +1,4 @@
-import { Grid, TextField, Button, Box, Alert } from "@mui/material";
+import { Grid, TextField, Button, Box, Alert, CircularProgress } from "@mui/material";
 import { useState } from 'react';
 import { useSendPasswordResetEmailMutation } from "../../services/userAuthApi";
 
@@ -8,7 +8,7 @@ const SendPasswordResetEmail = () => {
     msg: "",
     type: ""
   })
-  const [sendPasswordResetEmail]= useSendPasswordResetEmailMutation()
+  const [sendPasswordResetEmail, {isLoading}]= useSendPasswordResetEmailMutation()
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
@@ -35,9 +35,9 @@ const SendPasswordResetEmail = () => {
         <h1 >Reset Password</h1>
         <Box component='form' noValidate sx={{ mt: 1 }} id='password-reset-email-form' onSubmit={handleSubmit}>
           <TextField margin='normal' required fullWidth id='email' name='email' label='Email Address' />
-          <Box textAlign='center'>
+          {isLoading ? <CircularProgress/> :<Box textAlign='center'>
             <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }}>Send</Button>
-          </Box>
+          </Box>} 
           {error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ''}
         </Box>
       </Grid>
